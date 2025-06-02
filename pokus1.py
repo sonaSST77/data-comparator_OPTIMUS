75bdb9f4d5f588a327614d3d57bea941a4d52b84
sonaSST77 <sona.stradova@seznam.cz>
2025-06-02 11:03:03 +0200

    p5ipojeni k DB

--- pokus1.py
+++ pokus1.py
@@ -1,2 +1,30 @@
 import oracledb
 print("oracledb je nainstalováno správně")
+
+# Zadejte své údaje
+username = "so081267"
+password = "msaDBSona666666"
+dsn = "ocsxpptdb02r-scan.ux.to2cz.cz:1521/COMSA07R"  # např. "localhost:1521/COMSAR"
+
+try:
+    connection = oracledb.connect(
+        user=username,
+        password=password,
+        dsn=dsn
+    )
+    print("Připojení k databázi bylo úspěšné!")
+
+# Vytvoření kurzoru a provedení dotazu
+    cursor = connection.cursor()
+    cursor.execute("SELECT ID, DESCRIPTION  FROM MIGUSERP.LX_CETIN_PM_MIGRATION_WAVE lcpmw WHERE ID LIKE '2025%'")  # Změňte 'tabulka' na název vaší tabulky
+
+    # Výpis výsledků
+    for row in cursor:
+        print(row)
+
+    cursor.close()
+
+
+    connection.close()
+except Exception as e:
+    print("Chyba při připojení:", e)
