a5d3123b248e6b7a0e7897fb367357de1caf67d5
sonaSST77 <sona.stradova@seznam.cz>
2025-06-02 12:38:49 +0200

    Reoncil_storno_WAVE

--- Reoncil_storno_WAVE.py
+++ Reoncil_storno_WAVE.py
@@ -1,10 +1,16 @@
 import oracledb
+import pandas as pd
 print("oracledb je nainstalováno správně")
 
 # Zadejte své údaje
 username = "so081267"
 password = "msaDBSona666666"
 dsn = "ocsxpptdb02r-scan.ux.to2cz.cz:1521/COMSA07R"  # např. "localhost:1521/COMSAR"
+
+# Zadejte číslo vlny jako vstupní parametr
+cislo_vlny = input("Zadejte číslo vlny: ")  # zde můžete použít input() pro zadání od uživatele
+if not cislo_vlny:
+    cislo_vlny = "202506010001" #cervnova vlna
 
 try:
     connection = oracledb.connect(
@@ -16,17 +22,28 @@ try:
 
 # Vytvoření kurzoru a provedení dotazu
     cursor = connection.cursor()
-    cursor.execute(
-    "SELECT * FROM MIGUSERP.REP_REKONCIL_STAV_V_EDENIKU rrsve WHERE RRSVE.WAVE_ID IN ('202506010001') AND rrsve.STAV = 'storno' AND REPORT_DATE > TO_DATE('01-06-2025','DD-MM-YYYY')"
-    )  # Změňte 'tabulka' na název vaší tabulky
+    query = """
+    SELECT * FROM MIGUSERP.REP_REKONCIL_STAV_V_EDENIKU rrsve
+    WHERE RRSVE.WAVE_ID = :wave_id
+      AND rrsve.STAV = 'storno'
+      AND REPORT_DATE > TO_DATE('01-06-2025','DD-MM-YYYY')
+    """
+    cursor.execute(query, {"wave_id": cislo_vlny})
 
-    # Výpis výsledků
-    for row in cursor:
-        print(row)
+# Načtení výsledků do pandas DataFrame
+    columns = [col[0] for col in cursor.description]
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=columns)
+
+# Připravit DataFrame s textem dotazu
+    df_query = pd.DataFrame({"SQL dotaz": [query]})
+
+    # Uložení do Excelu na dva listy
+    with pd.ExcelWriter("vysledek.xlsx") as writer:
+        df.to_excel(writer, sheet_name="Výsledek", index=False)
+        df_query.to_excel(writer, sheet_name="SQL_dotaz", index=False)
 
     cursor.close()
-
-
     connection.close()
 except Exception as e:
     print("Chyba při připojení:", e)
