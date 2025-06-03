c4952e06aea03b5aa739cd9a6278a150e3128ad7
sonaSST77 <sona.stradova@seznam.cz>
2025-06-03 15:34:03 +0200

    Bey emailu

--- checkTodayReport.py
+++ checkTodayReport.py
@@ -10,12 +10,12 @@ password = "msaDBSona666666"
 dsn = "ocsxpptdb02r-scan.ux.to2cz.cz:1521/COMSA07R"  # např. "localhost:1521/COMSAR"
 
 # Nastavení e-mailu
-odesilatel = "sona.stradova@o2.cz"
-prijemce = "sona.stradova@o2.cz"  # Teams kanál má speciální e-mail, získejte jej v Teams
-smtp_server = "smtp.office365.com"   # nebo např. smtp.office365.com
-smtp_port = 587
-smtp_user = "sona.stradova@o2.cz"
-smtp_pass = "AjetuJaro2024"
+#odesilatel = "sona.stradova@o2.cz"
+#prijemce = "sona.stradova@o2.cz"  # Teams kanál má speciální e-mail, získejte jej v Teams
+#smtp_server = "smtp.office365.com"   # nebo např. smtp.office365.com
+#smtp_port = 587
+#smtp_user = "sona.stradova@o2.cz"
+#smtp_pass = "AjetuJaro2024"
 
 now = datetime.datetime.now()
 dnes = now.strftime("%d-%m-%Y")
@@ -44,18 +44,18 @@ try:
         datum = dnes
         print(f"Máme dnešní data v reportu, používá se dnešní datum!!!")
         # Odeslání e-mailu
-        subject = "Report: Data pro dnešní den jsou k dispozici"
-        body = f"V databázi jsou data pro datum {dnes}."
-        msg = MIMEText(body)
-        msg["Subject"] = subject
-        msg["From"] = odesilatel
-        msg["To"] = prijemce
+        #subject = "Report: Data pro dnešní den jsou k dispozici"
+        #body = f"V databázi jsou data pro datum {dnes}."
+        #msg = MIMEText(body)
+        #msg["Subject"] = subject
+        #msg["From"] = odesilatel
+        #msg["To"] = prijemce
 
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(smtp_user, smtp_pass)
-            server.sendmail(odesilatel, prijemce, msg.as_string())
-        print("E-mail byl odeslán.")
+        #with smtplib.SMTP(smtp_server, smtp_port) as server:
+        #    server.starttls()
+        #    server.login(smtp_user, smtp_pass)
+        #    server.sendmail(odesilatel, prijemce, msg.as_string())
+        #print("E-mail byl odeslán.")
     else:
         datum = vcera
         print(f"Nemáme dnešní data v reportu, budeme čekat!!!")
